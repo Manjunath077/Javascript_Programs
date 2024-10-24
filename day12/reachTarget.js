150c5cbf9e582de3d39aaef502680994296d0e7b
Manjunath077 <manjunathsaloki1915@gmail.com>
2024-10-24 22:53:38 +0530

    Third commit

---
+++ day12/reachTarget.js
@@ -0,0 +1,13 @@
+// Given a target N and a starting number 'A' check whether you can reach the target bu multiplying A by either 10 or 20 continuously
+// input : target = 1000
+
+function reachTarget(target){
+    if(number == target){
+        return true
+    }
+    if(number > target){
+        return false
+    }
+}
+let target = 1000
+console.log(reachTarget(target))
